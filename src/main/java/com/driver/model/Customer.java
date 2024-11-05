d001fab11d9deb3e3910fb8851f65f310d472482
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-11-05 20:41:51 +0530

    Update Customer.java

--- src/main/java/com/driver/model/Customer.java
+++ src/main/java/com/driver/model/Customer.java
@@ -1,24 +1,63 @@
-public class Customer {
-    private String name;
-    private String phoneNumber;
+package com.driver.model;
 
-    public Customer(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Customer")
+public class Customer{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int customerId;
+
+    String mobile;
+
+    String password;
+
+    //For Mapping
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    List<TripBooking> tripBookingList = new ArrayList<>();
+
+    public Customer() {
     }
 
-    // Getters and Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-
-    // Customer Functions
-    public void bookTrip(TripBooking tripBooking) {
-        // Logic to book a trip
+    public Customer(int customerId, String mobile, String password, List<TripBooking> tripBookingList) {
+        this.customerId = customerId;
+        this.mobile = mobile;
+        this.password = password;
+        this.tripBookingList = tripBookingList;
     }
 
-    public void deleteCustomer() {
-        // Logic to delete customer, e.g., remove from database
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<TripBooking> getTripBookingList() {
+        return tripBookingList;
+    }
+
+    public void setTripBookingList(List<TripBooking> tripBookingList) {
+        this.tripBookingList = tripBookingList;
     }
 }
