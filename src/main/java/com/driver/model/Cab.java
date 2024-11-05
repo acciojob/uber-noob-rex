769879697c98c0d48cec072b9f7a58e782ebabd0
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-11-05 20:41:07 +0530

    Update Cab.java

--- src/main/java/com/driver/model/Cab.java
+++ src/main/java/com/driver/model/Cab.java
@@ -1,15 +1,64 @@
-public class Cab {
-    private double perKmRate;
-    private boolean available;
+package com.driver.model;
 
-    public Cab(double perKmRate) {
-        this.perKmRate = perKmRate;
-        this.available = true;
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name = "Cab")
+public class Cab{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int Id;
+
+    int perKmRate;
+
+    boolean available;
+
+
+    //For mapping
+    @OneToOne
+    @JoinColumn
+    Driver driver;
+
+
+
+    public Cab() {
+
     }
 
-    // Getters and Setters
-    public double getPerKmRate() { return perKmRate; }
-    public void setPerKmRate(double perKmRate) { this.perKmRate = perKmRate; }
-    public boolean isAvailable() { return available; }
-    public void setAvailable(boolean available) { this.available = available; }
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver= driver;
+    }
+
+
 }
